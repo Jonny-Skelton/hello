51d2d2d73f8c88ec4656232e21d9160a49deb1b3
Jonny-Skelton <111836102+Jonny-Skelton@users.noreply.github.com>
2022-08-24 23:39:46 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Jonny Skelton
+ *         jskelton3@huskers.unl.edu
+ * Date: 2022/08/24
+ *
+ *
+ *
+ */
+
+#import <stdlib.h>
+#import <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Jonny!\n");
+
+  return 0;
+}
